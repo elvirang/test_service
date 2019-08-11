8b5b9dc94c8d6881e458713abee6b0c0656a23ff
Elvira Neganova <elviraneganova@MacBook-Pro-Elvira.local>
2019-08-11 18:09:58 +0500

    method changed

--- app.py
+++ app.py
@@ -32,17 +32,29 @@ application = Flask(__name__)  # Change assignment here
 
 
 #define loger func
+#def log(logger, json_params=None,step='new',internal_id=None):
+#    if json_params is None:
+#        logger.info('internal_id:{0} , step:{1}'.format(internal_id,step))
+#    else:
+#        logger.info('internal_id:{0} , step:{1} , message_id{2}'.format(internal_id,step,json_params['message_id']), extra={
+#          'json_params': json_params
+#        })
+#    
+#def log_2(logger, json_params=None,step='new',internal_id=None):
+#    if json_params is None:
+#        logger.info('internal_id:{0} , step:{1}'.format(internal_id,step))
+#    else:
+#        logger.info('internal_id:{0} , step:{1}'.format(internal_id,step), extra={
+#          'json_params': json_params
+#        })
+    
 def log(logger, json_params=None,step='new',internal_id=None):
     if json_params is None:
         logger.info('internal_id:{0} , step:{1}'.format(internal_id,step))
-    else:
+    elif 'message_id' in json_params:
         logger.info('internal_id:{0} , step:{1} , message_id{2}'.format(internal_id,step,json_params['message_id']), extra={
           'json_params': json_params
         })
-    
-def log_2(logger, json_params=None,step='new',internal_id=None):
-    if json_params is None:
-        logger.info('internal_id:{0} , step:{1}'.format(internal_id,step))
     else:
         logger.info('internal_id:{0} , step:{1}'.format(internal_id,step), extra={
           'json_params': json_params
@@ -131,10 +143,10 @@ def tolmachev_best():
                 'tolmachev_best_result':None
                 }
     try:
-        log_2(logger,step='new',internal_id=internal_id)
+        log(logger,step='new',internal_id=internal_id)
         getData = request.get_data()
         json_params = json.loads(getData) 
-        log_2(logger,json_params,'get json_params',internal_id)
+        log(logger,json_params,'get json_params',internal_id)
 
         #json_params = {
         #                'number':'1234',
@@ -148,7 +160,7 @@ def tolmachev_best():
            result *= int(digits[i])
         
         response['tolmachev_best_result'] = result
-        log_2(logger,json_params,'model done',internal_id)
+        log(logger,json_params,'model done',internal_id)
         status_code = 200
         
     except:
@@ -157,7 +169,7 @@ def tolmachev_best():
         traceback.print_exc()
         response['status'] = 'error'
         response['code'] = 501
-        log_2(logger,json_params,'some error',internal_id)
+        log(logger,json_params,'some error',internal_id)
 
     response = json.dumps(response)
     print(response)
